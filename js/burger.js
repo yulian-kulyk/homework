498bd727f6afef73be595c16e8f2584f66ce1b9d
Yulian Kulyk <ylian.game07@gmail.com>
2023-07-22 21:24:32 +0300

    menu

---
+++ js/burger.js
@@ -0,0 +1,22 @@
+(function () {
+    const iconBurger = document.querySelector(".navigation-burger");
+    const navigationsBody = document.querySelector(".navigation__background");
+    iconBurger.addEventListener("click", function (e) {
+      document.body.classList.toggle("lock");
+      iconBurger.classList.toggle("active");
+      navigationsBody.classList.toggle("active");
+    });
+  
+    const navLinks = document.querySelectorAll(".navigation__link");
+    navLinks.forEach((navLink) => {
+      navLink.addEventListener("click", (_) => {
+            if (iconBurger.classList.contains("active")) {
+                document.body.classList.remove("lock");
+                iconBurger.classList.remove("active");
+                navigationsBody.classList.remove("active");
+            }
+          }
+      );
+    });
+  })();
+  
