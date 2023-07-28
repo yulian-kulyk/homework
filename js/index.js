6427a0ed30037b12bf129d70a2221a9553f85edf
Yulian Kulyk <ylian.game07@gmail.com>
2023-07-28 12:50:50 +0300

    test js array

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const word= [
+    "dq",
+    "az",
+    "qs",
+    "we",
+    "eg",
+];
+const string = [] ;
+
+console.log(word.length);
+
+for (let i = 0; i < word.length; i++) {
+    console.log(string[i] = "Hi! " + word[i] + ".");
+}
